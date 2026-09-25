503fb849d5483a69922edf9d0c98f873beda2f8c
Claudio Fronterre <cla.f90@gmail.com>
2026-09-25 15:09:37 +0100

    Update Chapter 3 for current RiskMap API

--- R/CH3.R
+++ R/CH3.R
@@ -70,8 +70,8 @@ for (i in seq_len(n_kappa)) {
   fit_galicia_list[[i]] <- glgpm(formula_i,
                                  data = galicia, family = "gaussian",
                                  distance_units = "km", messages = FALSE)
-  llik_values[i] <- fit_galicia_list[[i]]$log.lik
-  sigma2_me_hat[i] <- coef(fit_galicia_list[[i]])["sigma2_me"]
+  llik_values[i] <- fit_galicia_list[[i]]$log_lik
+  sigma2_me_hat[i] <- coef(fit_galicia_list[[i]])$sigma2_me
 }
 
 galicia_kappa_profile <- list(
@@ -99,10 +99,11 @@ saveRDS(italy_fit, file = "data/italy_fit.rds")
 ## ------------------------------------------------------------
 data(anopheles)  # from RiskMap
 
-set.seed(1)
 an_fit <- glgpm(An.gambiae ~ elevation + gp(),
                 data = anopheles,
-                family = "poisson", messages = FALSE)
+                family = "poisson",
+                control_mcmc = set_control_mcmc(seed = 2026),
+                messages = FALSE)
 
 saveRDS(an_fit, file = "data/an_fit.rds")
 
@@ -117,20 +118,17 @@ liberia <- st_as_sf(liberia, coords = c("long", "lat"), crs = 4326)
 
 ## --- 6a. Main Binomial geostatistical model (no nugget), used
 ##         throughout Sec. "Example: river-blindness in Liberia"
-fit_liberia <-
-  glgpm(npos ~ log(elevation) + gp(),
-        den = ntest, data = liberia,
-        convert_to_crs = 32629,
-        control_mcmc = set_control_mcmc(seed = 2026),
-        return_samples = TRUE,
-        family = "binomial")
+fit_liberia <- glgpm(npos ~ log(elevation) + gp(),
+                     denominator = ntest, data = liberia,
+                     model_crs = 32629,
+                     control_mcmc = set_control_mcmc(seed = 2026),
+                     return_samples = TRUE,
+                     family = "binomial")
 
 saveRDS(fit_liberia, file = "data/fit_Liberia.rds")
 
-## --- 6b. Same model, saved under the name used later for the
-##         parametric-bootstrap section (identical specification
-##         to fit_liberia; kept as a separate object/name because
-##         that's the name simulate_glgpm()/bootstrap code expects)
+## --- 6b. Same model, saved under the name used later in the
+##         parametric-bootstrap section
 fit_liberia_no_nugget <- fit_liberia
 
 saveRDS(fit_liberia_no_nugget, file = "data/fit_liberia_no_nugget.rds")
@@ -138,19 +136,18 @@ saveRDS(fit_liberia_no_nugget, file = "data/fit_liberia_no_nugget.rds")
 ## --- 6c. Refit with the nugget estimated and a much larger
 ##         MCMC sample (110000 iterations, burn-in 10000, thin 10)
 par0_liberia <- coef(fit_liberia)
-par0_liberia$tau2 <- 0.1 
+par0_liberia$tau2 <- 0.1
 
-fit_liberia2 <-
-  glgpm(npos ~ log(elevation) + gp(nugget = TRUE),
-        den = ntest, data = liberia,
-        convert_to_crs = 32629,
-        par0 = par0_liberia,
-        control_mcmc = set_control_mcmc(n_sim = 110000,
-                                       burnin = 10000,
-                                       thin = 10,
-                                       seed = 2026),
-        return_samples = TRUE,
-        family = "binomial", messages = FALSE)
+fit_liberia2 <- glgpm(npos ~ log(elevation) + gp(nugget = TRUE),
+                      denominator = ntest, data = liberia,
+                      model_crs = 32629,
+                      par0 = par0_liberia,
+                      control_mcmc = set_control_mcmc(n_sim = 110000,
+                                                     burnin = 10000,
+                                                     thin = 10,
+                                                     seed = 2026),
+                      return_samples = TRUE,
+                      family = "binomial", messages = FALSE)
 
 saveRDS(fit_liberia2, file = "data/fit_liberia2.rds")
 
@@ -160,22 +157,24 @@ saveRDS(fit_liberia2, file = "data/fit_liberia2.rds")
 ## ------------------------------------------------------------
 n_sim <- 100  # increase to >= 1000 for production-quality CIs
 
-liberia_boot <- simulate_glgpm(n_sim = n_sim, model_fit = fit_liberia_no_nugget)
+liberia_boot <- simulate_glgpm(fit_liberia_no_nugget,
+                               nsim = n_sim,
+                               what = "data",
+                               seed = 2026)
 
-par_hat <- list()
+par_hat <- vector("list", n_sim)
 
 for (i in seq_len(n_sim)) {
-  liberia_boot$data_sim$npos_sim <-
-    liberia_boot$data_sim[[paste("npos_sim", i, sep = "")]]
-  
-  fit_sim <- glgpm(
-    formula = npos_sim ~ log(elevation) + gp(),
-    data = liberia_boot$data_sim,
-    family = "binomial",
-    par0 = coef(fit_liberia_no_nugget),
-    den = ntest
-  )
-  
+  sim_data <- simulated_data(liberia_boot, simulation = i)
+
+  fit_sim <- glgpm(formula = npos ~ log(elevation) + gp(),
+                   data = sim_data,
+                   family = "binomial",
+                   par0 = coef(fit_liberia_no_nugget),
+                   denominator = ntest,
+                   control_mcmc = set_control_mcmc(seed = 2026 + i),
+                   messages = FALSE)
+
   par_hat[[i]] <- coef(fit_sim)
 }
 
